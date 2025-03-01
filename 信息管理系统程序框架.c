c2840c07dab982dc6c8b0b4b71a10273ac4c900f
Bocchiid <487883857@qq.com>
2025-03-01 18:42:11 +0800

    Question_03 Finished

--- 信息管理系统程序框架.c
+++ 信息管理系统程序框架.c
@@ -15,7 +15,7 @@ typedef struct ElemType
 typedef struct Node
 {
 	elemType data;
-	struct node *next;
+	struct Node *next;
 } node;
 
 /**************************************
@@ -44,10 +44,63 @@ void printElement(elemType x)
 	printf("%s %s %s %.1f %.1f %.1f\n", x.id, x.name, x.sex, x.x, x.y, x.z);
 }
 
+/**************************************
+功能：初始化链表
+形参: node **head
+返回值：void
+日期：2025.03.01
+版本：v1
+作者: 韩贤煜
+***************************************/
+void init(node **head)
+{
+	*head = (node *)malloc(sizeof(node) * 1);
+	(*head)->next = NULL;
+}
+
+/**************************************
+功能：插入学生信息
+形参: node **tail, elemType x
+返回值：void
+日期：2025.03.01
+版本：v1
+作者: 韩贤煜
+***************************************/
+void insertElement(node **tail, elemType x)
+{
+	node *p = (node *)malloc(sizeof(node) * 1);
+	p->data = x;
+	p->next = NULL;
+	(*tail)->next = p;
+	(*tail) = p;
+}
+
+/**************************************
+功能：输出所有学生信息
+形参: node *head
+返回值：void
+日期：2025.03.01
+版本：v1
+作者: 韩贤煜
+***************************************/
+void printList(node *head)
+{
+	node *p = head->next;
+
+	while (p)
+	{
+		printElement(p->data);
+		p = p->next;
+	}
+}
+
 int main()
 {
 	elemType x;
 	// 1、链表或顺序表初始化
+	node *head;
+	init(&head);
+	node *tail = head;
 
 	while (1)
 	{
@@ -60,6 +113,7 @@ int main()
 			printf("Insert:\n");
 			// 3.1、输入学生信息
 			inputElement(&x);
+			insertElement(&tail, x);
 			printElement(x);
 			// 3.2如果成功插入信息（函数），则输出学生信息，
 			// 否则，输出 "Failed\n"
@@ -70,6 +124,7 @@ int main()
 			printf("List:\n");
 			// 4.1排序（函数）
 			// 4.2输出（函数）
+			printList(head);
 		}
 		// 5、修改
 		if (strcmp(op, "Change") == 0)
