33c24342d7e18621588d35a728fd59de6bcab086
Bocchiid <487883857@qq.com>
2025-03-01 18:58:24 +0800

    Quesiton_04 Finished

--- 信息管理系统程序框架.c
+++ 信息管理系统程序框架.c
@@ -19,12 +19,12 @@ typedef struct Node
 } node;
 
 /**************************************
-���ܣ�����ѧ����Ϣ
-�β�: elemType *x
-����ֵ��void
-���ڣ�2025.03.01
-�汾��v1
-����: ������
+功能：输入学生信息
+形参: elemType *x
+返回值：void
+日期：2025.03.01
+版本：v1
+作者: 韩贤煜
 ***************************************/
 void inputElement(elemType *x)
 {
@@ -32,71 +32,162 @@ void inputElement(elemType *x)
 }
 
 /**************************************
-���ܣ����ѧ����Ϣ
-�β�: elemType x
-����ֵ��void
-���ڣ�2025.03.01
-�汾��v1
-����: ������
+功能：输出学生信息
+形参: elemType x
+返回值：void
+日期：2025.03.01
+版本：v1
+作者: 韩贤煜
 ***************************************/
 void printElement(elemType x)
 {
 	printf("%s %s %s %.1f %.1f %.1f\n", x.id, x.name, x.sex, x.x, x.y, x.z);
 }
 
+/**************************************
+功能：初始化链表
+形参: node **head
+返回值：void
+日期：2025.03.01
+版本：v1
+作者: 韩贤煜
+***************************************/
+void init(node **head)
+{
+	*head = (node *)malloc(sizeof(node) * 1);
+	(*head)->next = NULL;
+}
+
+/**************************************
+功能：插入学生信息
+形参: node **tail, elemType x
+返回值：void
+日期：2025.03.01
+版本：v1
+作者: 韩贤煜
+***************************************/
+void insertElement(node **tail, elemType x)
+{
+	node *p = (node *)malloc(sizeof(node) * 1);
+	p->data = x;
+	p->next = NULL;
+	(*tail)->next = p;
+	(*tail) = p;
+}
+
+/**************************************
+功能：输出所有学生信息
+形参: node *head
+返回值：void
+日期：2025.03.01
+版本：v1
+作者: 韩贤煜
+***************************************/
+void printList(node *head)
+{
+	node *p = head->next;
+
+	while (p)
+	{
+		printElement(p->data);
+		p = p->next;
+	}
+}
+
+/**************************************
+功能：查找学生信息
+形参: node *head, char id[]
+返回值：node*
+日期：2025.03.01
+版本：v1
+作者: 韩贤煜
+***************************************/
+node *findElement(node *head, char id[])
+{
+	node *p = head->next;
+
+	while (p)
+	{
+		if (strcmp(p->data.id, id) == 0)
+			break;
+		p = p->next;
+	}
+
+	return p;
+}
+
 int main()
 {
 	elemType x;
-	// 1��������˳�����ʼ��
+	char id[20];
+	node *temp;
+	// 1、链表或顺序表初始化
+	node *head;
+	init(&head);
+	node *tail = head;
 
 	while (1)
 	{
 		char op[20];
-		// 2�����������ַ���
+		// 2、读入命令字符串
 		scanf("%s", op);
-		// 3����� ����
+		// 3、如果 插入
 		if (strcmp(op, "Insert") == 0)
 		{
 			printf("Insert:\n");
-			// 3.1������ѧ����Ϣ
+			// 3.1、输入学生信息
 			inputElement(&x);
+			insertElement(&tail, x);
 			printElement(x);
-			// 3.2����ɹ�������Ϣ���������������ѧ����Ϣ��
-			// ������� "Failed\n"
+			// 3.2如果成功插入信息（函数），则输出学生信息，
+			// 否则，输出 "Failed\n"
 		}
-		// 4��List
+		// 4、List
 		if (strcmp(op, "List") == 0)
 		{
 			printf("List:\n");
-			// 4.1���򣨺�����
-			// 4.2�����������
+			// 4.1排序（函数）
+			// 4.2输出（函数）
+			printList(head);
 		}
-		// 5���޸�
+		// 5、修改
 		if (strcmp(op, "Change") == 0)
 		{
 			printf("Change:\n");
-			// 5.1����ѧ����Ϣ
-			// 5.2����޸ĳɹ����������������ѧ����Ϣ
-			// ������� Failed
+			// 5.1输入学生信息
+			// 5.2如果修改成功（函数），则输出学生信息
+			// 否则输出 Failed
 		}
-		// 6��ɾ��ѧ����Ϣ
+		// 6、删除学生信息
 		if (strcmp(op, "Delete") == 0)
 		{
 			printf("Delete:\n");
-			// 6.1����ѧ��
-			// 6.2����ɹ�ɾ������������deleted
-			// ���� Failed
+			// 6.1输入学号
+			// 6.2如果成功删除（函数）则deleted
+			// 否则 Failed
 		}
-		// 7�����ң�
+		// 7、查找，
 		if (strcmp(op, "Find") == 0)
 		{
 			printf("Find:\n");
-			// ����ѧ��
-			// ������ҳɹ��������������ѧ����Ϣ���������Failed
+			// 输入学号
+			scanf("%s", id);
+			// 如果查找成功（函数），输出学生信息，否则，输出Failed
+			temp = findElement(head, id);
+
+			if (temp)
+			{
+				printElement(temp->data);
+			}
+			else
+			{
+				printf("Failed\n");
+			}
 		}
+
 		if (strcmp(op, "Quit") == 0)
 		{
-			printf("Good bye!");
+			printf("Good bye!\n");
 			break;
 		}
 	}
@@ -105,24 +196,24 @@ int main()
 }
 
 /**************************************
-���ܣ�
-�β�:
-����ֵ��
-���ڣ�
-�汾��
-����:
+功能：
+形参:
+返回值：
+日期：
+版本：
+作者:
 ***************************************/
 
 /**************************************
-��������Ŀ��ѧ����Ϣ����ϵͳ
-Ҫ�����ݽṹ�������������������ú���
-1. �������ѧ����Ϣ
-2. ��������
-3. �����Ĳ�����
-4. �����ı�������ӡ
-5. ɾ�����
-6. ���ҽ��
-7. �޸Ľ��
-8. ����
-9. ��������elemType��ֵ
+主函数项目：学生信息管理系统
+要求：数据结构必须用链表、尽可能用函数
+1. 输入输出学生信息
+2. 创建链表
+3. 链表的插入结点
+4. 链表的遍历、打印
+5. 删除结点
+6. 查找结点
+7. 修改结点
+8. 排序
+9. 交换二个elemType的值
 ****************************************/
